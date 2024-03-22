3245701b30433c99f9606897f9395bcccbee938f
Ubuntu <ubuntu@ip-172-31-8-54.us-east-2.compute.internal>
2024-03-22 20:53:19 +0000

    works for 80 but not api port

--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,7 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
-const port = 5000 
-
+const port = 3000;
 app.use('/users', createUserRouter(prismaClient))
 app.use('/contacts', createContactRouter(prismaClient))
 app.use('/companies', createCompanyRouter(prismaClient))
